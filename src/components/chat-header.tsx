6a107e4c47fadf8c97210280816473aab0ce1d49
madiha <madihazafer9@gmail.com>
2026-05-18 11:24:59 +0500

    file

--- src/components/chat-header.tsx
+++ src/components/chat-header.tsx
@@ -46,7 +46,7 @@ export function ChatHeader({
           
           <div className="flex items-center gap-2">
             <Avatar className="h-8 w-8">
-              <AvatarImage src="/asif.jpg" alt="User Avatar" />
+              <AvatarImage src="/1.png" alt="User Avatar" />
               <AvatarFallback>U</AvatarFallback>
             </Avatar>
             <span className="text-sm font-medium">Welcome, {USER_NAME}</span>
